9c20aef532ac0f6be1c806efab889cd7d503c289
Xavier Perret <xa.perret@protonmail.ch>
2021-04-29 22:36:41 +0200

    working on functions to generate in a nicer way activity element for main section

--- frontend/script.js
+++ frontend/script.js
@@ -21,3 +21,54 @@ function dataShow(data) {
   element.append(pAccessibility);
   mainSection.append(element);
 }
+
+function getBoredData(mode) {
+  fetch("http://www.boredapi.com/api/activity/")
+    .then((res) => res.json())
+    .then((data) => dataShow(data));
+
+  return data;
+}
+
+/**
+ * Creates an activity with given childClassName classes that is then added to given parentElement
+ * @param {*} parentElement   is the document parent element to which the child class needs to be linked
+ * @param {*} childClassName  is the document element classes name to attribute
+ *
+ * @return {} return the element that has been newly created
+ */
+function generateActivityHtml(parentElement, childClassName = "item activity") {
+  let element = document.createElement("div");
+  element.className = childClassName;
+
+  let activityPicture = document.createElement("img");
+  element.append(activityPicture);
+
+  let activityName = document.createElement("h1");
+  activityName.className = "activityName";
+  element.append(activityName);
+
+  let activityType = document.createElement("p");
+  activityType.className = "activityType";
+  element.append(activityType);
+
+  let activityParticipants = document.createElement("p");
+  activityParticipants.className = "activityParticipants";
+  element.append(activityParticipants);
+
+  let activityPrice = document.createElement("p");
+  activityPrice.className = "activityPrice";
+  element.append(activityPrice);
+
+  let activityAccess = document.createElement("p");
+  activityAccess.className = "activityAccess";
+  element.append(activityAccess);
+
+  parentElement.append(element);
+  return element;
+}
+
+function generateRandomActivity(activityElement) {
+  data = getBoredData(0);
+  activityElement.append(data.activity);
+}
